7a020eeb6013da4a2bb5e2cbc321c014b1a63d92
NancyAhmed97 <anancyahmed97@icloud.com>
2024-02-12 23:43:06 +0300

    zxcv

--- app.js
+++ app.js
@@ -33,10 +33,8 @@ mongoose.connect(process.env.CONNECTION_STRING)
     console.log(err);
 })
 
-
-
-app.listen(7000, ()=>{
+const port=process.env.PORT||7000
+app.listen(port, ()=>{
 
     console.log('server is running http://localhost:7000');
 })
-console.log('dfjkjmvfcdxcvml');
