5eef9e361fdd67a06f37b6a0e7de3b6b80182e22
Maraim Khaled <74541493+MARiam-2002@users.noreply.github.com>
2024-02-13 12:43:56 +0200

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -133,7 +133,7 @@ router.delete('/:id', (req, res)=>{
 })
 
 router.get(`/get/count`, async (req, res) =>{
-    const userCount = await User.countDocuments((count) => count)
+    const userCount = await User.countDocuments({count: count})
 
     if(!userCount) {
         res.status(500).json({success: false})
