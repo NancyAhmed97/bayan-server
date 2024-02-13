6f0e0c6b5aa49675205d5b0bf1bf0190450e5053
Maraim Khaled <74541493+MARiam-2002@users.noreply.github.com>
2024-02-13 21:31:57 +0200

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,4 @@
-const {User} = require('../Models/user');
+const User = require('../Models/user');
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
